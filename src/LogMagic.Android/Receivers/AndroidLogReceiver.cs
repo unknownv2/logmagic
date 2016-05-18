39fa90fb77ed07177ad440b804f96f9de1035466
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2016-05-18 16:41:28 +0100

    android log receiver

--- src/LogMagic.Android/Receivers/AndroidLogReceiver.cs
+++ src/LogMagic.Android/Receivers/AndroidLogReceiver.cs
@@ -1,42 +1,52 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+using System.Threading;
+using Android.Util;
 
 namespace LogMagic.Receivers
 {
-   /// <summary>
-   /// Uses standard Android logging
-   /// </summary>
-   public class AndroidLogReceiver : ILogReceiver
+   class AndroidLogReceiver : ILogReceiver
    {
-      /// <summary>
-      /// Create receiver with default formatter
-      /// </summary>
-      public AndroidLogReceiver()
-      {
-         
-      }
+      private readonly string _appName;
 
       /// <summary>
-      /// Sends chunk to the default Android logger
+      /// Create a new instance of log receiver
       /// </summary>
-      /// <param name="chunk"></param>
+      /// <param name="appName">Application name used as TAG in android logging. It's recommened to use app name so you can apply log
+      /// filtering based on it, instad of class name as Android is not that clever.</param>
+      public AndroidLogReceiver(string appName)
+      {
+         if(appName == null) throw new ArgumentNullException(nameof(appName));
+         _appName = appName;
+      }
+
       public void Send(LogChunk chunk)
       {
+         //this is the correct way to get current thread ID in Android
+         int androidThreadId = Thread.CurrentThread.ManagedThreadId;
 
+         string message = $"{androidThreadId}|{chunk.SourceName}|{chunk.Message}";
+         if(chunk.Error != null) message += $":{chunk.Error}";
+
+         Log.WriteLine(ToLogPriority(chunk.Severity), _appName, message);
       }
 
-      /// <summary>
-      /// Nothing to dispose
-      /// </summary>
+      private static LogPriority ToLogPriority(LogSeverity severity)
+      {
+         switch(severity)
+         {
+            case LogSeverity.Debug:
+               return LogPriority.Debug;
+            case LogSeverity.Error:
+               return LogPriority.Error;
+            case LogSeverity.Info:
+               return LogPriority.Error;
+            case LogSeverity.Warning:
+               return LogPriority.Warn;
+            default:
+               return LogPriority.Verbose;
+         }
+      }
+
       public void Dispose()
       {
       }
