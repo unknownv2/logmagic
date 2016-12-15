d5cc762db2a86252b913cc52467f7b0391366041
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2016-12-15 18:05:34 +0000

    removing dead code and moving existing tests, whcih won't run as they are not migrated from NUnit yet!

--- test/LogMagic.Test/Properties/AssemblyInfo.cs
+++ test/LogMagic.Test/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LogMagic.NetCore")]
+[assembly: AssemblyProduct("LogMagic.Test")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("de00e394-76a6-4bc0-adae-828f5c135c44")]
+[assembly: Guid("97b33270-5cce-40e0-a1c0-f6d93ff1f393")]
