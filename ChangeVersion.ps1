b20ca884f860845752f0838880fad86fbb343525
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-03-10 12:55:53 +0000

    add the version bumping script

---
+++ ChangeVersion.ps1
@@ -0,0 +1,28 @@
+param(
+   [String]
+   $Version
+)
+
+Write-Host "version is $Version"
+
+function Update-ProjectVersions([string]$RelPath, [string]$Version, [bool]$UpdatePackageVersion)
+{
+   $xml = [xml](Get-Content "$PSScriptRoot\$RelPath")
+
+   if($UpdatePackageVersion)
+   {
+      $xml.Project.PropertyGroup[0].VersionPrefix = $Version
+   }
+
+   foreach($other in $args)
+   {
+      $json.dependencies.$other = $Version
+
+      Write-Host "set $other to $Version"
+   }
+
+   $xml.Save("$PSScriptRoot\$RelPath")
+}
+
+Update-ProjectVersions "src\LogMagic\LogMagic.csproj" $Version $true
+Update-ProjectVersions "src\LogMagic.Microsoft.Azure\LogMagic.Microsoft.Azure.csproj" $Version $true
