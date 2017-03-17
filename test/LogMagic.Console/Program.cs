032c89f3da5abb98a68e267087ca5c4ce2f10ecd
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-03-17 15:10:40 +0000

    some testing

--- test/LogMagic.Console/Program.cs
+++ test/LogMagic.Console/Program.cs
@@ -13,11 +13,19 @@ namespace LogMagic.Console
       {
          //L.Config.WriteTo.Console();
          //L.Config.WriteTo.PoshConsole();
-         L.Config.WriteTo.File("c:\\tmp\\my.log");
+         //L.Config.WriteTo.File("c:\\tmp\\my.log");
+         L.Config
+            .WriteTo.AzureApplicationInsights("6fd5b7d6-5844-478b-a9df-cfb49d5bd65e")
+            .WriteTo.PoshConsole();
 
          log.D("debug message");
          log.W("warning message");
          log.I("information");
+
+         for (int i = 0; i < 1000; i++)
+         {
+            log.D("event at " + DateTime.UtcNow);
+         }
 
          System.Console.ReadLine();
       }
