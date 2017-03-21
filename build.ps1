f6f79c08b47719e60d4931e4ddd91ec2eba2ad61
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-03-21 20:30:56 +0000

    awesome versioning update

--- build.ps1
+++ build.ps1
@@ -6,28 +6,37 @@ param(
    $NuGetApiKey
 )
 
-$Version = "2.2.0-alpha-1"
+$VersionPrefix = "2"
+$VersionSuffix = "2.5.0"
+
 $SlnPath = "logmagic.sln"
+$AssemblyVersion = "$VersionPrefix.0.0.0"
+$PackageVersion = "$VersionPrefix.$VersionSuffix"
+Write-Host "version: $PackageVersion, assembly version: $AssemblyVersion"
 
 function Set-VstsBuildNumber($BuildNumber)
 {
    Write-Verbose -Verbose "##vso[build.updatebuildnumber]$BuildNumber"
 }
 
-function Update-ProjectVersion([string]$RelPath, [string]$Version)
+function Update-ProjectVersion([string]$Path)
 {
-   $xml = [xml](Get-Content "$PSScriptRoot\$RelPath")
+   $xml = [xml](Get-Content $Path)
 
    if($xml.Project.PropertyGroup.Count -eq $null)
    {
-      $xml.Project.PropertyGroup.VersionPrefix = $Version
+      $pg = $xml.Project.PropertyGroup
    }
    else
    {
-      $xml.Project.PropertyGroup[0].VersionPrefix = $Version
+      $pg = $xml.Project.PropertyGroup[0]
    }
 
-   $xml.Save("$PSScriptRoot\$RelPath")
+   $pg.Version = $PackageVersion
+   $pg.FileVersion = $PackageVersion
+   $pg.AssemblyVersion = $AssemblyVersion
+
+   $xml.Save($Path)
 }
 
 function Exec($Command)
@@ -48,17 +57,18 @@ if($Publish -and (-not $NuGetApiKey))
 }
 
 # Update versioning information
-Update-ProjectVersion "src\LogMagic\LogMagic.csproj" $Version
-Update-ProjectVersion "src\LogMagic.Microsoft.Azure\LogMagic.Microsoft.Azure.csproj" $Version
-Update-ProjectVersion "src\LogMagic.Microsoft.Azure.ServiceFabric\LogMagic.Microsoft.Azure.ServiceFabric.csproj" $Version
-Update-ProjectVersion "src\LogMagic.Microsoft.Azure.ApplicationInsights\LogMagic.Microsoft.Azure.ApplicationInsights.csproj" $Version
-Set-VstsBuildNumber $Version
+Get-ChildItem *.csproj -Recurse | Where-Object {-not($_.Name -like "*test*") -and -not($_.Name -like "*console*")} | % {
+   $path = $_.FullName
+   Write-Host "setting version on $path"
+   Update-ProjectVersion $path
+}
+Set-VstsBuildNumber $PackageVersion
 
 # Restore packages
 Exec "dotnet restore $SlnPath"
 
 # Build solution
-Get-ChildItem *.nupkg -Recurse | Remove-Item
+Get-ChildItem *.nupkg -Recurse | Remove-Item -Verbose
 Exec "dotnet build $SlnPath -c release"
 
 # Run the tests
@@ -69,8 +79,9 @@ if($Publish.IsPresent)
 {
    Write-Host "publishing nugets..."
 
-   Get-ChildItem *.nupkg -Recurse | % {
+   Get-ChildItem *.nupkg -Recurse | Where-Object {$_.FullName -like "*release*" } | % {
       $path = $_.FullName
+      Write-Host "publishing from $path"
 
       Exec "nuget push $path -Source https://www.nuget.org/api/v2/package -ApiKey $NuGetApiKey"
    }
