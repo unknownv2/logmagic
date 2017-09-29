c1c8554e7908e669e1a0d9f63e2398c05bafbee0
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-09-29 10:41:52 +0100

    add depenency extension overloads for synchronous execution

--- src/LogMagic/ILogExtensions.cs
+++ src/LogMagic/ILogExtensions.cs
@@ -64,5 +64,61 @@ namespace LogMagic
             }
          }
       }
+
+      /// <summary>
+      /// Track dependency automatically, measure time and handle exceptions
+      /// </summary>
+      /// <typeparam name="T"></typeparam>
+      /// <param name="log">The log.</param>
+      /// <param name="type">The type.</param>
+      /// <param name="name">The name.</param>
+      /// <param name="command">The command.</param>
+      /// <param name="call">The call.</param>
+      /// <param name="properties">Extra properties</param>
+      public static T Dependency<T>(this ILog log, string type, string name, string command, Func<T> call, params KeyValuePair<string, object>[] properties)
+      {
+         using (var time = new TimeMeasure())
+         {
+            try
+            {
+               T result = call();
+               log.Dependency(type, name, command, time.ElapsedTicks, null, properties);
+               return result;
+            }
+            catch (Exception ex)
+            {
+               log.Dependency(type, name, command, time.ElapsedTicks, ex, properties);
+               throw;
+            }
+         }
+      }
+
+      /// <summary>
+      /// Track dependency automatically, measure time and handle exceptions
+      /// </summary>
+      /// <typeparam name="T"></typeparam>
+      /// <param name="log">The log.</param>
+      /// <param name="type">The type.</param>
+      /// <param name="name">The name.</param>
+      /// <param name="command">The command.</param>
+      /// <param name="call">The call.</param>
+      /// <param name="properties">Extra properties</param>
+      public static void Dependency(this ILog log, string type, string name, string command, Action call, params KeyValuePair<string, object>[] properties)
+      {
+         using (var time = new TimeMeasure())
+         {
+            try
+            {
+               call();
+               log.Dependency(type, name, command, time.ElapsedTicks, null, properties);
+            }
+            catch (Exception ex)
+            {
+               log.Dependency(type, name, command, time.ElapsedTicks, ex, properties);
+               throw;
+            }
+         }
+      }
+
    }
 }
